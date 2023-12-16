d2148a7a34e1970515b2fd997741357fd8eaae33
bishar <bishar.dahir@student.moringaschool.com>
2023-12-16 17:17:27 +0300

    completed assignment

---
+++ index.js
@@ -0,0 +1,11 @@
+
+
+function divide() {
+  return 2000 / 100;
+}
+
+// Arrow function to square a number
+const square = (num) => num * num;
+
+// Arrow function to add two numbers
+const add = (a, b) => a + b;
